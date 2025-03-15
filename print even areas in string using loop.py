27db51e9a77e956c648008e49916a460a8c796c2
codewithbarath <devbarath4602@gmail.com>
2025-03-15 18:55:17 +0530

    Add files via upload

---
+++ print even areas in string using loop.py
@@ -0,0 +1,18 @@
+''''''''''
+Input:
+str = DoctorPhenomenal
+Output:
+DcoPeoea
+'''''''''
+a="doctorphenomenal"
+for i in range(0,len(a),2):
+    print(a[i],end="")
+
+
+x=int(input("enter x:"))
+
+
+while(x>0):
+    y=x-1
+    x=x-1
+    print(y)    
